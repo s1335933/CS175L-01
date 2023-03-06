ffa434b717f6ff4fa3a65b452088c14730c9573f
s1335933 <114097171+s1335933@users.noreply.github.com>
2023-03-06 12:25:05 -0500

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -3,15 +3,34 @@
 #CS-175L-01
 #AverageFromInput
 
-total = 0
-count = 0
+def read_numbers(filename):
+    numbers = []
+    try:
+        with open("numbers.txt", "r") as file:
+            for line in file:
+                try:
+                    num = float(line.strip())
+                    numbers.append(num)
+                    print("I read in {:2d} number(s) Current number is: {:7.2f} Total is: {:7.2f}".format(len(numbers), num, sum(numbers)))
+                except ValueError:
+                    print(f"Could not convert line '{line.strip()}' to a number.")
+    except IOError:
+        print(f"Could not open file {filename}.")
+    return numbers
 
-with open("numbers.txt", "r") as file:
-    for line in file:
-        num = float(line.strip())
-        count += 1
-        total += num
-        print(f"I read in {count} number(s) Current number is: {num:.2f} Total is: {total:.2f}")
+def calculate_average(numbers):
+    if numbers:
+        return sum(numbers) / len(numbers)
+    else:
+        return 0
 
-average = total / count
-print(f"Average: {average:.1f}")
+def print_average(average):
+    print("Average: {:7.1f}". format(average))
+
+def main():
+    numbers = read_numbers("numbers.txt")
+    average = calculate_average(numbers)
+    print_average(average)
+
+if __name__=='__main__':
+    main()
